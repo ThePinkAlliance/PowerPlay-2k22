37d5cc3de9d6e606b37ac800e57d11c4e06d1a5e
devsamuelv <realalloutmusic@gmail.com>
2022-12-07 22:51:01 -0500

    more auto changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TestAuto.java
@@ -1,28 +1,173 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+import java.util.List;
 
 @Autonomous()
 public class TestAuto extends LinearOpMode {
-    Pose2d startingPose = new Pose2d(11.5, 0);
+    Pose2d startingPose = new Pose2d(-69, -11.7);
+
+    TrajectorySequence selectedSequence;
+
+    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
+    // private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/CustomTeamModel.tflite";
+    private static final String VUFORIA_KEY =
+            "AU5HdoL/////AAABmdflEYY1uEgKvLLnXhuUKQEiOh/Swf8w1NP3fjwJ0L5KhNZjEBmtqvcb1vRriuL7dxpTimmKsrPxVN0GSemDm1z0zZHiuEDJjN6is0gE5cC8eCf5/w4A9J9xygAQMiK4UOje3lWQjKpyMbqNeKgy1I6PZqyXBae1+6/gecIRmHuDjcqGFcEnRKmf8e6iPrFIdaC53DkmQUxJWRalVEqWsdmwmLm69AsaoG+aL7D0xkupVo7U23C2fdDkl66qsFO7v7jf0ONGEdmNjg1TTEKQmrip86/iMst+I7mdLA/pYsY00EjAjgPJ8YdXEqR5pKR2CK4DNmVU+c2A7T+w+KhGwxJ8us9j9FpYTd1yC0wRQD0R";
+
+    private static final String[] LABELS = {
+            "1 Bolt",
+            "2 Bulb",
+            "3 Panel"
+    };
+    private enum OBJECT_LABEL {
+        NONE(""),
+        BOLT("1 Bolt"),
+        BULB("2 Bulb"),
+        PANEL("3 Panel");
+
+        String name;
+
+        OBJECT_LABEL(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
+
+
 
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Trajectory locationTwo = drive
-                .trajectoryBuilder(startingPose).lineTo(new Vector2d(startingPose.getX(), 34.5)).build();
-        Trajectory locationOne = drive.trajectoryBuilder(startingPose).splineTo(new Vector2d(startingPose.getX(), 34.5), 0).splineTo(new Vector2d(startingPose.getX() + 23, 34.5), 0).build();
-//        Trajectory locationThree = drive.trajectoryBuilder(startingPose).lineTo(new Vector2d(startingPose.getX(), 34.5)).lineTo(new Vector2d(startingPose.getX() - 23, 34.5)).build();
+        drive.setPoseEstimate(startingPose);
+
+        TrajectorySequence left = drive.trajectorySequenceBuilder(startingPose).forward(34.5).strafeLeft(34.5).build();
+        TrajectorySequence right = drive.trajectorySequenceBuilder(startingPose).forward(34.5).strafeRight(34.5).build();
+        TrajectorySequence center = drive.trajectorySequenceBuilder(startingPose).forward(34.5).build();
+
+        initVuforia();
+        initTfod();
+
+        /**
+         * Activate TensorFlow Object Detection before we wait for the start command.
+         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+         **/
+        if (tfod != null) {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can increase the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 16/9).
+//            tfod.setZoom(1.0, 16.0 / 9.0);
+        }
+
+        while (opModeInInit()) {
+            if (tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+
+                if (updatedRecognitions != null) {
+                    telemetry.addData("::", updatedRecognitions.size());
+                    telemetry.update();
+                }
+
+                if (updatedRecognitions != null && !updatedRecognitions.isEmpty()) {
+                    telemetry.addData("# Objects Detected", updatedRecognitions.size());
+
+                    String recognitionName = updatedRecognitions.get(0).getLabel();
+
+                    telemetry.addData("rec name", recognitionName);
+                    telemetry.update();
+                }
+
+//                switch (foundTag) {
+//                    case BULB:
+//                        selectedSequence = center;
+//                        break;
+//                    case PANEL:
+//                        selectedSequence = left;
+//                        break;
+//
+//                    case BOLT:
+//                        selectedSequence = right;
+//                        break;
+//                }
+            }
+        }
 
         waitForStart();
 
-        drive.followTrajectory(locationOne);
+        if (selectedSequence == null) {
+            selectedSequence = center;
+        }
+
+//        drive.followTrajectorySequence(selectedSequence);
+    }
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.75f;
+        tfodParameters.isModelTensorFlow2 = true;
+
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+
+        // Use loadModelFromAsset() if the TF Model is built in as an asset by Android Studio
+        // Use loadModelFromFile() if you have downloaded a custom team model to the Robot Controller's FLASH.
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+//         tfod.loadModelFromFile(TFOD_MODEL_FILE, LABELS);
     }
 }
